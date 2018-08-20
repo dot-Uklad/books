f4efc8e5480b34e38f513f634f26f4facd879119
s2426 <39331554+s2426@users.noreply.github.com>
2018-08-20 07:17:04 +0200

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,15 @@
+var settings;
+
+(function() {
+    'use strict';
+
+    settings = {
+      title:        '.Układ Books',
+      baseurl:       'http://uklad.eu.org', // your base url useful for subpath, if any.
+      
+      sharing_link: 'https://drive.google.com/drive/folders/1P-Uo8eBwhBfpngbUTk3-k5H8jsXe4P-g?usp=sharing',
+
+      CLIENT_ID:    '1029418226097-0riqh3nck6akm6tohg6unh59m30g0004.apps.googleusercontent.com',
+      SCOPES:       'https://www.googleapis.com/auth/drive',
+    };
+})();
